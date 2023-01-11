ec44247976444ca721c2c1e08213886ee771966c
= <=>
2023-01-11 20:04:39 +0000

    Fix

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- Create table if dose not exists users on the holberton database
 
 CREATE TABLE users(
-	id INTEGER PRIMARY KEY,
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255)
 );
