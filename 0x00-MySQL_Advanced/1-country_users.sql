b543eae01e3f6a1fcba47f9fbb19f9f42c466b23
= <=>
2023-01-11 20:16:02 +0000

    Create script

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Create the table users to not fail on any device
+-- Use ENUM instead of VARCHAR
+CREATE TABLE IF NOT EXISTS users(
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM("US","CO","TN") NOT NULL DEFAULT "US"
+	);
