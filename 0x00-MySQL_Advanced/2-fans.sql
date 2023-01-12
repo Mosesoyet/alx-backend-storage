66fd0191d6af241df9267950e3cd9845c83f34f6
= <=>
2023-01-12 16:14:03 +0000

    Modify

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- SQL script that ranks country origins of bands, ordered by the number of (non-unique) fans
-SELECT COUNT(id) AS total_band_members, origin
+SELECT origin, COUNT(fans)
 FROM metal_bands
 GROUP BY origin
-ORDER BY COUNT(id) DESC;
+ORDER BY COUNT(fans) DESC;
