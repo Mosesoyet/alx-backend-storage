ed5688511e5f5e4520d9049468223931082380cd
= <=>
2023-01-12 19:00:00 +0000

    Add script to validdate email

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,10 @@
+-- script that creates a trigger to validate email
+DELIMITER $$ ;
+CREATE TRIGGER validate BEFORE UPDATE ON users
+FOR EACH ROW
+	BEGIN
+		IF NEW.email != OLD.email THEN
+			SET NEW.valid_email = 0;
+		END IF;
+	END;$$
+DELIMITER ;
