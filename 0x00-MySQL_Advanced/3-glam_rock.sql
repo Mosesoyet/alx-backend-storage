7e34444d53e6683cc0d9dc5a9f872320063164a7
= <=>
2023-01-12 18:45:25 +0000

    Update SQL Keywards to Upper Cases

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- script to filter bands by type
 
-SELECT band_name, COALESCE(split, 2020) - formed as lifespan
+SELECT band_name, COALESCE(split, 2020) - formed AS lifespan
 FROM metal_bands
 WHERE style LIKE "%Glam rock%"
 ORDER BY lifespan DESC;
