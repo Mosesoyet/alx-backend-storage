0e51b844ddf86e5105821b730363f45bc28c3a6b
= <=>
2023-01-12 18:37:48 +0000

    Fix syntax error by adding '%' at the end of LIKE QUERY STRING

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- script to filter bands by type
 
-SELECT band_name, COALESCE(split, 2020) - formed as lifespan
+SELECT band_name, COALESCE(split, 2023) - formed as lifespan
 FROM metal_bands
-WHERE style LIKE "%Glam rock"
+WHERE style LIKE "%Glam rock%"
 ORDER BY lifespan DESC;
