f46bb00ca3c838a1c16434f5a4d08b74e66530ac
= <=>
2023-01-13 16:43:15 +0000

    Modify

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -3,5 +3,4 @@
 
 SELECT band_name, COALESCE(split, 2020) - formed AS lifespan FROM
 metal_bands WHERE
-style LIKE "%Glam rock%"
-ORDER BY lifespan DESC;
+style LIKE "%Glam rock%" ORDER BY lifespan DESC;
