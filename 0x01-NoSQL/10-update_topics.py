7b2283340431090b0bd187d7acaafa578d0d85c6
= <=>
2023-01-17 21:46:27 +0000

    Modify

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -11,6 +11,5 @@ def update_topics(mongo_collection, name, topics):
     """
     return mongo_collection.update_many(
             {"name": name},
-            {"$set": {"topic": topics}},
-            {"multi": True}
+            {"$set": [{"topic": topics}]}
             )
