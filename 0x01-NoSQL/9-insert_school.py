97066faf5ef81a2372a20b2c52456173f0244962
= <=>
2023-01-17 20:56:25 +0000

    Script to add data

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+task 9 modules
+"""
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Insert in school
+    """
+    return mongo_collection.insert_one(kwargs).inserted_id
