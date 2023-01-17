db69fa2df2bd31d188b71aa335a8e1414def86cb
= <=>
2023-01-17 21:48:12 +0000

    Fix spelling error in code

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -11,5 +11,5 @@ def update_topics(mongo_collection, name, topics):
     """
     return mongo_collection.update_many(
             {"name": name},
-            {"$set": [{"topic": topics}]}
+            {"$set": {"topics": topics}}
             )
