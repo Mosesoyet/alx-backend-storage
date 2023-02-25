8801789bbc07591a4f1ec9bb103bff7f7440dcea
= <=>
2023-02-25 03:25:50 -0800

    Modify

--- 0x00-MySQL_Advanced/101-students.py
+++ 0x00-MySQL_Advanced/101-students.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
-""" task 101 module
-"""
+'''Task 14's module.
+'''
 
 
 def top_students(mongo_collection):
-    """Prints all students in a collection sorted by average score.
-    """
+    '''Prints all students in a collection sorted by average score.
+    '''
     students = mongo_collection.aggregate(
         [
             {
-                "$project": {
+                '$project': {
                     '_id': 1,
                     'name': 1,
                     'averageScore': {
