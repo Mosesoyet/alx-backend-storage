5a390d667553dcff6c59c1ac65520e5a52fbb75c
= <=>
2023-02-25 02:52:16 -0800

    Modify

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,7 @@
 -- script to filter bands by type
 -- SELECT band_name then use COALESCE() to find the year
 
-SELECT band_name, COALESCE(split, 2020) - formed AS lifespan FROM
-metal_bands WHERE
-style LIKE "%Glam rock%" ORDER BY lifespan DESC;
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
